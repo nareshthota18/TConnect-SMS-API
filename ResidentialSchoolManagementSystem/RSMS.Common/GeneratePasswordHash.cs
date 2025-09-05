d9af5f591a97a9f58ebb30ae40ddc955eadf261a
PraneethPagunta09 <praneethpagunta@tconnectservices.com>
2025-09-06 03:07:22 +0530

    password hash common class

---
+++ ResidentialSchoolManagementSystem/RSMS.Common/GeneratePasswordHash.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSMS.Common
+{
+    public static class GeneratePasswordHash
+    {
+        public static (byte[] Hash, byte[] Salt) GetPasswordHash(string password)
+        {
+            // ex : string password = "Super@123"; 
+
+            using (var hmac = new HMACSHA512())
+            {
+                byte[] salt = hmac.Key; // HMAC key = salt
+                byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return (hash, salt);
+            }
+        }
+    }
+}
+
