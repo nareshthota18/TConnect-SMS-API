44836200a2d614c8a6a89fbf6337323f7d539dcb
PraneethPagunta09 <praneethpagunta@tconnectservices.com>
2025-11-27 12:02:10 +0530

    ItemType as part of inventory

--- ResidentialSchoolManagementSystem/RSMS.Common/DTO/InventoryDTO.cs
+++ ResidentialSchoolManagementSystem/RSMS.Common/DTO/InventoryDTO.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public Guid RSHostelId { get; set; }
         public Guid ItemId { get; set; }
+        public Guid ItemTypeId { get; set; }
         public decimal OpeningBalance { get; set; }
         public decimal QuantityReceived { get; set; }
         public decimal QuantityIssued { get; set; }
